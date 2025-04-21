28159ccbac49172364fd11f5e0b93afeaa2ef37c
Vadym.Zakharchuk <vadym.zakharchuk@proximaresearch.com>
2025-04-21 08:35:45 +0300

    Refactor chat input area for improved styling and functionality
    Adjusted layout and styles for better alignment and flexibility. Refactored event handling for clarity and consistency, including introducing `triggerFileInput` and modifying `handleSendMessage` calls. Cleaned up unnecessary comments and optimized component structure.

--- src/components/chat-window/ChatWindow.vue
+++ src/components/chat-window/ChatWindow.vue
@@ -11,7 +11,7 @@
           <img  v-if="senderAvatar(message.payload.senderId)"
                 :src="senderAvatar(message.payload.senderId)"
                 alt="avatar"
-          class="w-10 h-10 rounded-full mr-2"/>
+                class="w-10 h-10 rounded-full mr-2"/>
           <span class="font-semibold mr-2">{{ senderName(message.payload.senderId) }}:</span>
         </div>
 
@@ -44,13 +44,13 @@
       </div>
     </div>
     <div class="chat-inputArea">
-      <div class="flex justify-center">
+      <div class="flex justify-center items-center space-x-2">
         <input
           type="text"
           v-model="newMessageText"
-          @keyup.enter="handleSendMessage"
+          @keyup.enter="() => handleSendMessage()"
           placeholder="Enter message..."
-          class="chat-inputArea__input"
+          class="chat-inputArea__input flex-grow"
         />
         <div>
           <input
@@ -61,20 +61,12 @@
             accept="image/*, video/*, application/*"
             multiple
           />
-          <button @click="$refs.fileInput.click()" class="chat-inputArea__attach">
-            <svg width="28px" height="28px" viewBox="0 0 28 28"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                 xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier">
-                <title>attachment 2</title> <desc>Created with Sketch Beta.</desc> <defs> </defs>
-                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
-                  <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-260.000000, -156.000000)" fill="#000000">
-                    <path d="M286.562,166.181 L272.325,180.26 C269.966,182.593 266.141,182.593 263.782,180.26 C261.423,177.928 261.423,174.146 263.782,171.813 L276.596,159.141 C278.168,157.586 280.718,157.586 282.291,159.141 C283.863,160.696 283.863,163.218 282.291,164.772 L269.477,177.444 C268.691,178.222 267.416,178.222 266.629,177.444 C265.843,176.667 265.843,175.406 266.629,174.628 L278.02,163.365 L276.596,161.957 L265.206,173.221 C263.633,174.775 263.633,177.297 265.206,178.853 C266.778,180.407 269.328,180.407 270.901,178.852 L283.714,166.181 C286.073,163.849 286.074,160.065 283.715,157.733 C281.355,155.4 277.531,155.4 275.172,157.733 L261.646,171.108 L261.696,171.157 C259.238,174.281 259.455,178.797 262.358,181.668 C265.262,184.539 269.828,184.754 272.987,182.323 L273.036,182.372 L287.986,167.589 L286.562,166.181" id="attachment-2" sketch:type="MSShapeGroup">
-              </path> </g> </g> </g>
-            </svg>
+          <button @click="triggerFileInput" class="chat-inputArea__attach mt-1">
+            <svg height="32px" width="32px" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 280.067 280.067" xml:space="preserve" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path style="fill:#D07C40;" d="M149.823,257.142c-31.398,30.698-81.882,30.576-113.105-0.429 c-31.214-30.987-31.337-81.129-0.42-112.308l-0.026-0.018L149.841,31.615l14.203-14.098c23.522-23.356,61.65-23.356,85.172,0 s23.522,61.221,0,84.586l-125.19,123.02l-0.044-0.035c-15.428,14.771-40.018,14.666-55.262-0.394 c-15.244-15.069-15.34-39.361-0.394-54.588l-0.044-0.053l13.94-13.756l69.701-68.843l13.931,13.774l-83.632,82.599 c-7.701,7.596-7.701,19.926,0,27.53s20.188,7.604,27.88,0L235.02,87.987l-0.035-0.026l0.473-0.403 c15.682-15.568,15.682-40.823,0-56.39s-41.094-15.568-56.776,0l-0.42,0.473l-0.026-0.018l-14.194,14.089L50.466,158.485 c-23.522,23.356-23.522,61.221,0,84.577s61.659,23.356,85.163,0l99.375-98.675l14.194-14.089l14.194,14.089l-14.194,14.098 l-99.357,98.675C149.841,257.159,149.823,257.142,149.823,257.142z"></path> </g> </g></svg>
           </button>
         </div>
         <button
-          @click="handleSendMessage()"
+          @click="() => handleSendMessage()"
           class="chat-inputArea__btn"
         >
           Send
@@ -149,8 +141,6 @@ const handleSendMessage = (file?: File) => {
 const handleFileSelect = (event: Event) => {
   const files = (event.target as HTMLInputElement).files;
   if (files && files.length > 0) {
-    // Оскільки ми дозволяємо multiple, можна відправити кілька файлів.
-    // Зараз ми обробляємо лише перший вибраний файл.
     handleSendMessage(files[0]);
   }
 }
@@ -191,18 +181,16 @@ const senderAvatar = (senderId: string) => {
     @apply p-4 border-t border-gray-200;
 
     &__input {
-      @apply bg-blue-200 text-black rounded-r-md px-4 py-2 font-medium hover:bg-blue-400;
-      @apply w-4/5 focus:outline-none focus:ring-2 focus:ring-blue-500;
+      @apply bg-blue-200 text-black rounded-r-md px-4 py-2 font-medium hover:bg-blue-400 flex-grow;
+      @apply focus:outline-none focus:ring-2 focus:ring-blue-500;
     }
     &__attach {
-      @apply ml-2 w-8 h-8;
+      @apply ml-2 w-8 h-8 cursor-pointer;
     }
 
     &__btn {
-      @apply ml-4 bg-gray-200 hover:bg-gray-300 text-gray-700 font-semibold;
-      @apply w-1/5 py-2 px-4 rounded;
+      @apply ml-2 bg-gray-200 hover:bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded-r-md;
     }
   }
 }
-
 </style>
